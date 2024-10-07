58b5a22cf39077bc40e726c0c60201dc348ab2d8
MirzaHB <hassan.baig@ucalgary.ca>
2024-10-07 10:42:26 -0600

    reading the input file

---
+++ fileReader.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fileReader.h"
+#include "DLL.h"
+
+int readInputFile(const char *filename, DllList *list)
+{
+    FILE *input = fopen(filename, "r");
+    if (input == NULL)
+    {
+        fprintf(stderr, "Error opening input file: %s\n", filename);
+        return -1;
+    }
+
+    char line[256];
+    int rowCount = 0;
+
+    if (fgets(line, sizeof(line), input) == NULL)
+    {
+        fprintf(stderr, "Error reading header line\n");
+        fclose(input);
+        return -1;
+    }
+
+    while (fgets(line, sizeof(line), input))
+    {
+
+        line[strcspn(line, "\n")] = '\0';
+
+        char *token;
+        int pageNum;
+        int dirty;
+
+        token = strtok(line, ",");
+        if (token == NULL)
+        {
+            fprintf(stderr, "Error parsing page number in line: %s\n", line);
+            continue;
+        }
+        pageNum = atoi(token);
+
+        token = strtok(NULL, ",");
+        if (token == NULL)
+        {
+            fprintf(stderr, "Error parsing dirty bit in line: %s\n", line);
+            continue;
+        }
+        dirty = atoi(token);
+
+        // Append to the list
+        append(list, pageNum, dirty);
+        rowCount++;
+    }
+
+    fclose(input);
+    return rowCount;
+}
---
+++ fileReader.h
@@ -0,0 +1,8 @@
+#ifndef FILEREADER_H
+#define FILEREADER_H
+
+#include "DLL.h"
+
+int readInputFile(const char *filename, DllList *list);
+
+#endif
