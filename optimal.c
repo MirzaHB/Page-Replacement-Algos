2a658621998ebf5e91394b3b93918e175b1f96a0
MirzaHB <hassan.baig@ucalgary.ca>
2024-10-14 21:23:00 -0600

    faster optimal algo

--- optimal.c
+++ optimal.c
@@ -1,24 +1,10 @@
+// optimal.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 #include "optimal.h"
+#include <string.h>
 #include "DLL.h"
-
-// find next reference of page in reference list
-int findNextUse(Node *currentRef, int pageNum, int currentPosition, int totalReferences)
-{
-    int position = currentPosition + 1;
-    while (currentRef != NULL && position < totalReferences)
-    {
-        if (currentRef->pageNum == pageNum)
-        {
-            return position;
-        }
-        currentRef = currentRef->nextNode;
-        position++;
-    }
-    // If the page is not found in the future references, return a large number
-    return 99999;
-}
 
 void Optimal(DllList *referenceList)
 {
@@ -32,33 +18,94 @@ void Optimal(DllList *referenceList)
         tempRef = tempRef->nextNode;
     }
 
+    /*
+        referenceArray will hold the sequence of the pages numbers as they are refrenced
+        and nextUse will hold when each page in referenceArray will be used again in teh future
+        dirtybit array just to make sure dirtybit updates for writebacks are happening correctly
+    */
+    int *referenceArray = (int *)malloc(sizeof(int) * totalReferences);
+    int *dirtyArray = (int *)malloc(sizeof(int) * totalReferences);
+    int *nextUse = (int *)malloc(sizeof(int) * totalReferences);
+
+    // create lastSeen array
+    int lastSeen[TOTAL_PAGES];
+    memset(lastSeen, -1, sizeof(lastSeen));
+
+    // populate referenceArray and dirtyArray
+    tempRef = referenceList->left->nextNode;
+    int index = 0;
+    while (tempRef != referenceList->right)
+    {
+        int pageNum = tempRef->pageNum;
+        if (pageNum >= TOTAL_PAGES)
+        {
+            fprintf(stderr, "Page number %d exceeds MAX_PAGE_NUM %d. Increase MAX_PAGE_NUM.\n", pageNum, TOTAL_PAGES);
+            exit(EXIT_FAILURE);
+        }
+        referenceArray[index] = pageNum;
+        dirtyArray[index] = tempRef->dirty;
+        index++;
+        tempRef = tempRef->nextNode;
+    }
+
+    // build nextUse array with help of referenceArray
+    for (int i = totalReferences - 1; i >= 0; i--)
+    {
+        int pageNum = referenceArray[i];
+        nextUse[i] = lastSeen[pageNum];
+        lastSeen[pageNum] = i;
+    }
+
+    // Arrays that track pages in ram and whenn they will be used next
+    int inRAM[TOTAL_PAGES];
+    int currentNextUse[TOTAL_PAGES];
+
     for (int frameCount = 1; frameCount <= MAX_FRAMES; frameCount++)
     {
         int pageFaults = 0;
         int writeBacks = 0;
         int framesInUse = 0;
-        int currentPosition = 0;
 
-        // this represents ram
+        // should prob just use memsetfor this
+        for (int i = 0; i < TOTAL_PAGES; i++)
+        {
+            inRAM[i] = 0;
+            currentNextUse[i] = -1;
+        }
+
+        // creating ram
         DllList *frameList = createList();
 
-        Node *currentRef = referenceList->left->nextNode;
-
-        while (currentRef != referenceList->right)
+        for (int currentPosition = 0; currentPosition < totalReferences; currentPosition++)
         {
-            int pageNum = currentRef->pageNum;
-            int dirty = currentRef->dirty;
+            int pageNum = referenceArray[currentPosition];
+            int dirty = dirtyArray[currentPosition];
 
-            // search for the page in RAM
-            Node *pageInFrames = Search(frameList, pageNum);
-
-            if (pageInFrames != NULL)
+            // figure out when page is used again next
+            int nextUsePosition = nextUse[currentPosition];
+            if (nextUsePosition == -1)
             {
-                // Page is already in RAM update dirty bit if necessary
+                nextUsePosition = INT_MAX; // high num cause page is not used again
+            }
+
+            if (inRAM[pageNum])
+            {
+                // Page is in RAM
+                Node *pageInFrames = Search(frameList, pageNum);
+                if (pageInFrames == NULL)
+                {
+                    fprintf(stderr, "Error: Page %d is marked in RAM but not found in frame list.\n", pageNum);
+                    exit(EXIT_FAILURE);
+                }
+
+                // Update dirty bit if necessary
                 if (dirty == 1 && pageInFrames->dirty == 0)
                 {
                     pageInFrames->dirty = 1;
                 }
+
+                // uodate the nextUseposistion
+                currentNextUse[pageNum] = nextUsePosition;
             }
             else
             {
@@ -67,49 +114,62 @@ void Optimal(DllList *referenceList)
 
                 if (framesInUse < frameCount)
                 {
-                    // space in RAM, add the page
+                    // sapce in ram so add referenced page
                     append(frameList, pageNum, dirty);
+                    inRAM[pageNum] = 1;
+                    currentNextUse[pageNum] = nextUsePosition;
                     framesInUse++;
                 }
                 else
                 {
-                    Node *currentFrameNode = frameList->left->nextNode;
-                    Node *pageToReplace = NULL;
+                    // ram is full; need to replace a page
                     int farthestNextUse = -1;
+                    int pageToReplaceNum = -1;
+                    Node *pageToReplaceNode = NULL;
 
-                    while (currentFrameNode != frameList->right)
+                    // find page to replace
+                    Node *currentNode = frameList->left->nextNode;
+                    while (currentNode != frameList->right)
                     {
-                        int nextUsePosition = findNextUse(currentRef->nextNode, currentFrameNode->pageNum, currentPosition, totalReferences);
-                        if (nextUsePosition > farthestNextUse)
+                        int nodePageNum = currentNode->pageNum;
+                        int nodeNextUse = currentNextUse[nodePageNum];
+
+                        if (nodeNextUse > farthestNextUse)
                         {
-                            farthestNextUse = nextUsePosition;
-                            pageToReplace = currentFrameNode;
+                            farthestNextUse = nodeNextUse;
+                            pageToReplaceNum = nodePageNum;
+                            pageToReplaceNode = currentNode;
                         }
-                        currentFrameNode = currentFrameNode->nextNode;
+
+                        currentNode = currentNode->nextNode;
                     }
 
-                    // replace the pageToReplace with the new page
-                    if (pageToReplace->dirty == 1)
+                    // increment writebacks if needed for page to be replaced
+                    if (pageToReplaceNode->dirty == 1)
                     {
                         writeBacks++;
                     }
+                    // remove page from ram
+                    removeNode(frameList, pageToReplaceNode);
+                    inRAM[pageToReplaceNum] = 0;
+                    currentNextUse[pageToReplaceNum] = -1;
 
-                    // remove the page to replace from RAM
-                    removeNode(frameList, pageToReplace);
-
-                    // add the new page to RAM
+                    // add the new page
                     append(frameList, pageNum, dirty);
+                    inRAM[pageNum] = 1;
+                    currentNextUse[pageNum] = nextUsePosition;
                 }
             }
-
-            currentRef = currentRef->nextNode;
-            currentPosition++;
         }
 
-        // output results for current frame count
         printf("%d\t%d\t\t%d\n", frameCount, pageFaults, writeBacks);
 
-        // garbage collection cause heap usage
+        // garbage collection for ram
         freeList(frameList);
     }
+
+    // garbage collection for all the helper arrays on heap
+    free(referenceArray);
+    free(dirtyArray);
+    free(nextUse);
 }
